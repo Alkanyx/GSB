a951986c5a6c0160002273203d1ca80a911e76f2
rodrigues <rodrigues@J205-09.infogestion.local>
2016-11-16 14:52:20 +0100

    menu

--- vues/v_menu.php
+++ vues/v_menu.php
@@ -1,15 +1,15 @@
-<div name="haut" style="margin: 2 2 2 2 ;height:6%;"><h1><img src="images/logo.jpg" width="100" height="60"/>Gestion des visites</h1></div>
+<div name="haut" style="margin: 2 2 2 2 ;height:6%;"><h1><a href="index.php"><img src="images/logo.jpg" width="100" height="60"/></a>Gestion des visites</h1></div>
 <div name="gauche" style="float:left;width:18%; background-color:white; height:100%;">
 	<h2>Outils</h2>
 	<ul><li>Comptes-Rendus</li>
 		<ul>
-			<li><a href="formRAPPORT_VISITE.htm" >Nouveaux</a></li>
+			<li><a href="index.php?uc=consulter&action=formRap" >Nouveaux</a></li>
 			<li>Consulter</li>
 		</ul>
 		<li>Consulter</li>
-		<ul><li><a href="formMEDICAMENT.htm" >Médicaments</a></li>
-			<li><a href="formPRATICIEN.htm" >Praticiens</a></li>
-			<li><a href="formVISITEUR.htm" >Autres visiteurs</a></li>
+		<ul><li><a href="index.php?uc=consulter&action=formMed" >Médicaments</a></li>
+			<li><a href="index.php?uc=consulter&action=formPrac" >Praticiens</a></li>
+			<li><a href="index.php?uc=consulter&action=formVis" >Autres visiteurs</a></li>
 		</ul>
 	</ul>
 </div>
