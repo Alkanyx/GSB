d69325cc5b4cbcf9966289929805fe7d523f30d8
zickler <zickler@J205-10.infogestion.local>
2016-11-16 15:53:37 +0100

    aaa

--- vues/v_entete.php
+++ vues/v_entete.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
-  <head>
-    <title> GSB Medicament</title>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
- 	<link href="./styles/styles.css" rel="stylesheet" type="text/css" /> 
-    <link rel="shortcut icon" type="image" href="./images/logo.jpg" />
-  </head>
-  <body>
-        <h1>Accueil de GSB</h1>
+<head>
+<title>GSB Medicament</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link href="./styles/styles.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" type="image" href="./images/logo.jpg" />
+</head>
+<body>
+	<h1>Accueil de GSB</h1>
--- vues/v_formVisiteur.php
+++ vues/v_formVisiteur.php
@@ -1,24 +1,23 @@
 <title>formulaire VISITEUR</title>
-	<style type="text/css">
-		<!-- body {background-color: white; color:5599EE; } 
-			.titre { width : 180 ;  clear:left; float:left; } 
-			.zone { width : 30car ; float : left; color:7091BB } -->
-	</style>
-</head>
-<body>
-<div name="droite" style="float:left;width:80%;">
-	<div name="bas" style="margin : 10 2 2 2;clear:left;background-color:77AADD;color:white;height:88%;">
-	<form name="formVISITEUR" method="post" action="recupVISITEUR.php">
-		<h1> Visiteurs </h1>
-		<select name="lstDept" class="titre"><option value="">Département</option><option value="01">Ain</option></select>
-		<select name="lstVisiteur" class="zone"><option value="a131">Villechalane</option></select>
-		<label class="titre">NOM :</label><input type="text" size="25" name="VIS_NOM" class="zone" />
-		<label class="titre">PRENOM :</label><input type="text" size="50" name="Vis_PRENOM" class="zone" />
-		<label class="titre">ADRESSE :</label><input type="text" size="50" name="VIS_ADRESSE" class="zone" />
-		<label class="titre">CP :</label><input type="text" size="5" name="VIS_CP" class="zone" />
-		<label class="titre">VILLE :</label><input type="text" size="30" name="VIS_VILLE" class="zone" />
-		<label class="titre">SECTEUR :</label><input type="text" size="1" name="SEC_CODE" class="zone" />
-		<label class="titre">&nbsp;</label><input class="zone"type="button" value="<"></input><input class="zone"type="button" value=">"></input>
-	</form>
+
+<div name="droite" style="float: left; width: 80%;">
+	<div name="bas" class="bas">
+		<form name="formVISITEUR" method="post" action="recupVISITEUR.php">
+			<h1>Visiteurs</h1>
+			<select name="lstDept" class="titre"><option value="">Département</option>
+				<option value="01">Ain</option></select> <select name="lstVisiteur"
+				class="zone"><option value="a131">Villechalane</option></select> <label
+				class="titre">NOM :</label><input type="text" size="25"
+				name="VIS_NOM" class="zone" /> <label class="titre">PRENOM :</label><input
+				type="text" size="50" name="Vis_PRENOM" class="zone" /> <label
+				class="titre">ADRESSE :</label><input type="text" size="50"
+				name="VIS_ADRESSE" class="zone" /> <label class="titre">CP :</label><input
+				type="text" size="5" name="VIS_CP" class="zone" /> <label
+				class="titre">VILLE :</label><input type="text" size="30"
+				name="VIS_VILLE" class="zone" /> <label class="titre">SECTEUR :</label><input
+				type="text" size="1" name="SEC_CODE" class="zone" /> <label
+				class="titre">&nbsp;</label><input class="zone" type="button"
+				value="<"></input><input class="zone" type="button" value=">"></input>
+		</form>
 	</div>
 </div>
