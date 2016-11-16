af504b167281e2a9685360574aab0f3f6d1574ec
zickler <zickler@J205-10.infogestion.local>
2016-11-16 13:51:05 +0100

    teee

--- vues/v_entete.php
+++ vues/v_entete.php
@@ -2,7 +2,10 @@
        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
   <head>
-    <p>GSB medicaments</p>
+    <title> GSB Medicament</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link href="./styles/styles.css" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico" />
   </head>
   <body>
         <h1>Accueil de GSB</h1>
