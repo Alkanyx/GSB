3202dc2da4b65ee13f5465ad2da15372c84d290d
rodrigues <asca@dajdaj.fr>
2016-11-30 15:38:40 +0100

    suce

--- controleur/c_saisir.php
+++ controleur/c_saisir.php
@@ -1,5 +1,13 @@
 <?php
-
-$curseur = $laBase->requeteSelect('select * from praticien where pra_num='.$_POST["pratNum"]);
-
+if(!isset($_REQUEST['action'])){
+	$_REQUEST['action'] = '';
+}
+$action = $_REQUEST['action'];
+switch($action){
+	case 'saisirRap':{
+		$lesPrac=$pdo->getPrac();
+		include("vues/v_formPracticien.php");
+		break;
+	}	
+}
 ?>
