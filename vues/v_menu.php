cf1f7014507212668eae5d47888f0fc571618556
rodrigues <asca@dajdaj.fr>
2016-11-30 16:05:47 +0100

    a

--- vues/v_menu.php
+++ vues/v_menu.php
@@ -25,10 +25,6 @@
 		<ul>
 			<li><a href="index.php?uc=consulter&action=formMed">Médicaments</a></li>
 		</ul>
-		<li>Saisir</li>
-		<ul>
-			<li><a href="index.php?uc=consulter&action=formMed">Médicaments</a></li>
-		</ul>
 		<li>Connexion</li>
 		<ul>
 			<li><a href="index.php?uc=connexion&action=deconnexion">Déconnexion</a></li>
