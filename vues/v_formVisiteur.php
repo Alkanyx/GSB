25cada293659159ecec48d2f35f10d59096265ce
zickler <zickler@J205-10.infogestion.local>
2016-12-14 14:55:59 +0100

    commit première étape consulter rapport visite

--- vues/v_formVisiteur.php
+++ vues/v_formVisiteur.php
@@ -9,17 +9,19 @@
 		<form name="formListeRecherche">
 			<table>
 				<tr>
-					<td width="50%" align="right" style="color:white;"><label>Date de début: </label></td>
+					<td width="50%" align="right" style="color: white;"><label>Date de
+							début: </label></td>
 					<td align="left"><input type="text" class="calendrier"
 						name="dateDebut" id="dateDebut" value="" required /></td>
 				</tr>
 				<tr>
-					<td width="50%" align="right" style="color:white;"><label>Date de fin: </label></td>
+					<td width="50%" align="right" style="color: white;"><label>Date de
+							fin: </label></td>
 					<td align="left"><input type="text" class="calendrier"
 						name="dateFin" id="dateFin" value="" required /></td>
 				</tr>
 				<select name="select" id="selectPrac"
-				onChange="javascript:location.href = this.value;">
+					onChange="javascript:location.href = this.value;">
 		
 		<?php
 		if (! isset ( $_REQUEST ['practicien'] )) {
@@ -27,12 +29,21 @@
 		} else {
 			echo '<option value=0>' . $lePracticien ['PRA_NOM'] . ' ' . $lePracticien ['PRA_PRENOM'] . '</option>';
 		}
+		
 		foreach ( $lesPracticiens as $unPracticien ) {
-			if ($unPracticien ['PRA_NUM'] != $lePracticien ['PRA_NUM']) {
-				echo '
+			echo '
 			<option value=index.php?uc=consulter&action=formPrac&practicien=' . $unPracticien ['PRA_NUM'] . '>' . $unPracticien ['PRA_NOM'] . ' ' . $unPracticien ['PRA_PRENOM'] . '</option>';
-			}
 		}
+		
+		?>
+			
+			
+			
+			
+			
+			
+			
+			
 			</table>
 		</form>
 	</div>
